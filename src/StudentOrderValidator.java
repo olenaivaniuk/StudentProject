860d915b1da2a006dba422efc26fe77fcc60c361
olenaivaniuk <oivaniuk.scout24@gmail.com>
2019-08-02 16:07:09 +0200

    Project for Java course

---
+++ src/StudentOrderValidator.java
@@ -0,0 +1,29 @@
+public class StudentOrderValidator {
+
+    public static void main(String[] args) {
+        checkAll();
+    }
+
+    static void checkAll() {
+        checkCityRegister();
+        checkWedding();
+        checkChildren();
+        checkStudent();
+        }
+
+    static void checkCityRegister() {
+        System.out.println("cityRegister is running");
+    }
+
+    static void checkWedding() {
+        System.out.println("checkWedding is running");
+    }
+
+    static void checkChildren() {
+        System.out.println("checkChildren is running");
+    }
+
+    static void checkStudent() {
+        System.out.println("checkStudent is running");
+    }
+}

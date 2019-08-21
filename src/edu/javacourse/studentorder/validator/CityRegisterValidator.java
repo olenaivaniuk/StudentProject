bca8cf75f7c85d332c55683263a83e7164331f3b
olenaivaniuk <oivaniuk.scout24@gmail.com>
2019-08-21 21:25:08 +0200

    Exceptions

--- src/edu/javacourse/studentorder/validator/CityRegisterValidator.java
+++ src/edu/javacourse/studentorder/validator/CityRegisterValidator.java
@@ -60,6 +60,11 @@ public class CityRegisterValidator {
             status = AnswerCityRegisterItem.CityStatus.ERROR;
             error = new AnswerCityRegisterItem.CityError(IN_CODE, ex.getMessage());
         }
+        catch (Exception ex) {
+            ex.printStackTrace(System.out);
+            status = AnswerCityRegisterItem.CityStatus.ERROR;
+            error = new AnswerCityRegisterItem.CityError(IN_CODE, ex.getMessage());
+        }
         AnswerCityRegisterItem ans = new AnswerCityRegisterItem(status, person, error);
         return null;
     }
